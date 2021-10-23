2536c98cb00d5a8f3d0286bc3b843c0879acc4e9
stefanmez94 <stefanmeza93@gmail.com>
2021-10-23 23:34:54 +0200

    Add comments

--- script.js
+++ script.js
@@ -12,34 +12,22 @@ const muted = document.querySelector('.js-muted');
 const fullScreen = document.querySelector('.js-fs-button');
 
 
-function playVideo() {
-	if (playBtn.querySelector('i').classList.contains('fa-play')) {
-		playBtn.querySelector('i').classList.remove('fa-play');
-		playBtn.querySelector('i').classList.add('fa-pause');
-		video.play();
-	} else {
-		playBtn.querySelector('i').classList.remove('fa-pause')
-		playBtn.querySelector('i').classList.add('fa-play');
-		video.pause();
-	}
-}
-
-function seekTo(position) {
-	video.currentTime = position;
-}
-
+// regulate speed of the video
 function speedPlayback(speed) {
 	video.playbackRate = speed;
 }
 
+// fill progres bar base on the current time from video
 function updateProgress() {
 	progressBar.value = (video.currentTime / video.duration) * 100;
 }
 
+// change video current time when click on progress bar
 function setVideoProgress() {
 	video.currentTime = (+progressBar.value * video.duration) / 100;
 }
 
+// change play/pause button 
 playBtn.addEventListener('click', function() {
 	let playPause = this.querySelector('i');
 	
@@ -56,7 +44,7 @@ playBtn.addEventListener('click', function() {
 	}
 });
 
-
+// add funtionality on speedy buttons based on data atribute in html
 speeds.forEach(speed => {
 	speed.addEventListener('click', function(e) {
 		// console.log(e.target.dataset.speed);
@@ -64,24 +52,26 @@ speeds.forEach(speed => {
 	});
 });
 
-// progressBar.addEventListener('change', setVideoProgress)
 
 video.addEventListener('timeupdate', updateProgress);
 progressBar.addEventListener('change', setVideoProgress)
 
+// show controls when hover over video
 videoWrapper.addEventListener('mouseover', function() {
 	controlsWrapper.classList.add('active');
 });
 
+// hide controls when leave video
 videoWrapper.addEventListener('mouseout', function() {
 	controlsWrapper.classList.remove('active');
 });
 
+// set video volume
 volumeSlider.addEventListener('change', function() {
 	video.volume = volumeSlider.value / 100;
 })
 
-
+// mute video and set volume slider based on current volume
 muted.addEventListener('click', function() {
 	let currentVolume = video.volume;	
 	
@@ -96,6 +86,7 @@ muted.addEventListener('click', function() {
 	}
 })
 
+// enter full screen mode
 fullScreen.addEventListener('click', function() {
 	video.requestFullscreen();
 })
